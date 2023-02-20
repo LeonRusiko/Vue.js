2544faa8a16ac5bf86414c8e7c6a1505fed5a18e
Aituar Semeibek <113921403+LeonRusiko@users.noreply.github.com>
2023-02-21 00:27:29 +0600

    Add files via upload

---
+++ Lesson5/src/Diagram.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+  props:['allData']
+}
+</script>
+
+<template>
+<div class="diagram-back">
+  <div class="diagram-item" v-for="data in allData" :key="data.index">
+    <p class="item-name">{{data.catName}}</p>
+    <div  :style="{width: data.catPercent + 'px', height: 5 + 'px', backgroundColor: '#16a085'}"></div>
+  </div>
+</div>
+</template>
+
+<style>
+  p{
+    margin: 0;
+  }
+  .diagram-back{
+    width: 250px;
+    min-height: 100px;
+    border: 1px solid black;
+  }
+  .item-name{
+    width: 100px;
+  }
+  .diagram-item{
+    display: flex;
+    align-items: center;
+  }
+</style>
